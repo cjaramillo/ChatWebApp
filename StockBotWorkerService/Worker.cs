bafa09dd32590827d01340886361a79739e9aa49
cesar.jaramillo <cesar.jaramillo@easysoft.com.ec>
2020-08-29 17:05:45 -0500

    Fix on invalid command flag stock code

--- StockBotWorkerService/Worker.cs
+++ StockBotWorkerService/Worker.cs
@@ -19,7 +19,6 @@ namespace StockBotWorkerService
         private readonly IConfiguration _configuration;
         private Invoker _invoker;
         private Parser _parser;
-        private RpcServer _rpcServer;
 
         public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
@@ -27,80 +26,74 @@ namespace StockBotWorkerService
             _configuration = configuration;
             _invoker = new Invoker(configuration); 
             _parser = new Parser(configuration);
-            _rpcServer = new RpcServer(configuration);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
-            //while (!stoppingToken.IsCancellationRequested)
             string queueName = _configuration["QueueConfiguration:Name"];
             string queueHostName = _configuration["QueueConfiguration:HostName"];
-
             var factory = new ConnectionFactory() { HostName = queueHostName };
-            //using (var connection = factory.CreateConnection())
-            //using (var channel = connection.CreateModel())
             var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: queueName, durable: false,
+                exclusive: false, autoDelete: false, arguments: null);
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: queueName,
+                autoAck: false, consumer: consumer);
+            consumer.Received += (model, ea) =>
             {
-                channel.QueueDeclare(queue: queueName, durable: false,
-                    exclusive: false, autoDelete: false, arguments: null);
-                channel.BasicQos(0, 1, false);
-                var consumer = new EventingBasicConsumer(channel);
-                channel.BasicConsume(queue: queueName,
-                    autoAck: false, consumer: consumer);
-                Console.WriteLine(" [x] Awaiting RPC requests");
-
-                consumer.Received += (model, ea) =>
+                string response = string.Empty;
+                var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replyProps = channel.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
+                try
                 {
-                    string response = null;
-
-                    var body = ea.Body.ToArray();
-                    var props = ea.BasicProperties;
-                    var replyProps = channel.CreateBasicProperties();
-                    replyProps.CorrelationId = props.CorrelationId;
-                    try
+                    string[] commandSplitted;
+                    var message = Encoding.UTF8.GetString(body);
+                    if (message.StartsWith("/stock="))
                     {
-                        string[] commandSplitted;
-                        var message = Encoding.UTF8.GetString(body);
-                        if (message.StartsWith("/stock="))
+                        commandSplitted = message.Split("/stock=");
+                        string stockCode = commandSplitted[1];
+                        if (string.IsNullOrEmpty(stockCode))
                         {
-                            commandSplitted = message.Split("/stock=");
-                            if (commandSplitted.Length < 2)
+                            response = "Stock code is required.";
+                        }
+                        else 
+                        {
+                            string apiResponse = _invoker.InvokeApi(commandSplitted[1]);
+                            var parsedResponse = _parser.ParseContent(apiResponse);
+                            if (parsedResponse.ContainsKey("Symbol") && parsedResponse.ContainsKey("Close"))
                             {
-                                response = "Stock code is required.";
-                            }
-                            else 
-                            {
-                                string apiResponse = _invoker.InvokeApi(commandSplitted[1]);
-                                var parsedResponse = _parser.ParseContent(apiResponse);
                                 response = $"{parsedResponse["Symbol"]} quote is ${parsedResponse["Close"]} per share";
                             }
+                            else
+                            {
+                                response = "Stock quote API response could not be recognized";
+                            }   
                         }
-                        else
-                        {
-                            response = "Command not recognized";
-                        }
                     }
-                    catch (Exception e)
+                    else
                     {
-                        Console.WriteLine(" [.] " + e.Message);
-                        response = "";
+                        response = "Command not recognized";
                     }
-                    finally
-                    {
-                        var responseBytes = Encoding.UTF8.GetBytes(response);
-                        channel.BasicPublish(exchange: "", routingKey: props.ReplyTo,
-                            basicProperties: replyProps, body: responseBytes);
-                        channel.BasicAck(deliveryTag: ea.DeliveryTag,
-                            multiple: false);
-                    }
-                };
-            }
-            
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError($"Message: {e.Message}\nStacktrace: {e.StackTrace}");
+                }
+                finally
+                {
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+                    channel.BasicPublish(exchange: "", routingKey: props.ReplyTo,
+                        basicProperties: replyProps, body: responseBytes);
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                        multiple: false);
+                }
+            };
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            //await Task.Delay(1000, stoppingToken);
-            
+            _logger.LogInformation(" [x] Awaiting RPC requests");
         }
     }
 }
