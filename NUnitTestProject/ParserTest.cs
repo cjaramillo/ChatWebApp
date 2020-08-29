bc09de099922af0dea5e2f92a4b167341f073c84
cesar.jaramillo <cesar.jaramillo@easysoft.com.ec>
2020-08-29 16:55:13 -0500

    Adding unit testing

---
+++ NUnitTestProject/ParserTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using StockBotWorkerService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject
+{
+    public class ParserTest
+    {
+        private IConfiguration _configuration;
+
+        [SetUp]
+        public void Setup()
+        {
+            _configuration = BuildConfiguration(TestContext.CurrentContext.TestDirectory);
+            
+        }
+
+        [Test]
+        public void ParseWithOkContent()
+        {
+            Parser parser = new Parser(_configuration);
+            Dictionary<string, string> parsedDict = parser.ParseContent("Symbol,Date,Time,Open,High,Low,Close,Volume\r\nAAPL.US,2020-08-27,22:00:02,508.57,509.94,495.33,500.04,38744808\r\n");
+            Assert.AreEqual(parsedDict["Symbol"], "AAPL.US");
+            Assert.AreEqual(parsedDict["Date"], "2020-08-27");
+            Assert.AreEqual(parsedDict["Time"], "22:00:02");
+            Assert.AreEqual(parsedDict["Open"], "508.57");
+            Assert.AreEqual(parsedDict["High"], "509.94");
+            Assert.AreEqual(parsedDict["Low"], "495.33");
+            Assert.AreEqual(parsedDict["Close"], "500.04");
+            Assert.AreEqual(parsedDict["Volume"], "38744808");
+        }
+
+        [Test]
+        public void ParseWithEmptyContent()
+        {
+            Parser parser = new Parser(_configuration);
+            Dictionary<string, string> parsedDict = parser.ParseContent(string.Empty);
+            Assert.AreEqual(parsedDict.Count, 0);
+        }
+
+        public IConfigurationRoot BuildConfiguration(string testDirectory)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(testDirectory)
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+        }
+    }
+}
---
+++ NUnitTestProject/RpcClientTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using RPCComunicator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject
+{
+    public class RpcClientTest
+    {
+        private IConfiguration _configuration;
+
+        [SetUp]
+        public void Setup()
+        {
+            _configuration = BuildConfiguration(TestContext.CurrentContext.TestDirectory);
+
+        }
+
+        [Test]
+        public void RpcCallOnline()
+        {
+            RpcClient rpcClient = new RpcClient(_configuration);
+            string response = rpcClient.Call("/stock=aapl.us");
+            Assert.IsTrue(response.ToUpper().Contains("AAPL.US"));
+        }
+
+
+
+        public IConfigurationRoot BuildConfiguration(string testDirectory)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(testDirectory)
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+        }
+
+    }
+}
